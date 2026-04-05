6ff79815153ffd8e7b4d25150ebfe8c44106532c
o-tab <o-tab@dg.dg>
2026-04-05 14:20:55 +0700

    updates

--- assets/js/smoothScroll.js
+++ assets/js/smoothScroll.js
@@ -536,9 +536,3 @@ const ScrollManager = {
 };
 
 ScrollManager.init();
-
-// Делаем объект глобальным, чтобы другие скрипты его видели
-window.ScrollManager = ScrollManager;
-
-// Сообщаем всему документу, что скролл-менеджер готов к работе
-document.dispatchEvent(new CustomEvent('scrollManagerReady'));
