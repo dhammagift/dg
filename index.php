b2a48fc8933cab719ff88203979e0736c610b20c
o-yoga <o28@yoga>
2026-04-16 07:58:36 +0500

    added Privacy Policy for Google

--- index.php
+++ index.php
@@ -1265,13 +1265,19 @@ foreach ($slides as $index => $slide) {
 		
         <!-- Copyright Section-->
 <div id="copyright" class="copyright py-4 text-center text-white " >
-<div class="container"><a target="_blank" rel="license" href="<?php echo $cclink; ?>" title="<?php echo $cctitle; ?>"><img alt="Creative Commons License" style="border-width:0" src="/assets/img/88x31.png" loading="lazy" /></a><small> Copyright <a class="text-white text-decoration-none" href="/assets/readylinebyline.html">&copy;</a> Dhamma.Gift <?php echo $mode; ?> <a class="text-white text-decoration-none" href="/assets/countdowntable.php">2022</a>-<?php echo date("Y"); ?></small>  <small id="copyrightnote">
+<div class="container"><a target="_blank" rel="license" href="<?php echo $cclink; ?>" title="<?php echo $cctitle; ?>"><img alt="Creative Commons License" style="border-width:0" src="/assets/img/88x31.png" loading="lazy" /></a><small> Copyright <a class="text-white text-decoration-none" href="/assets/readylinebyline.html">&copy;</a> Dhamma.Gift <?php echo $mode; ?> <a class="text-white text-decoration-none" href="/assets/countdowntable.php">2022</a>-<?php echo date("Y"); ?> <?php echo $privacy; ?></small>  
+
+<!-- <small id="copyrightnote">
  
 <p class="text-muted text-center mx-auto max-w-650">
    <?php echo $copyrightnote; ?> 
 </p>
 
-</small> <button class="btn btn-secondary text-center installButton" id="" style="display:none;"><?php echo $installpwa;?></button></div>
+</small> -->
+
+ <button class="btn btn-secondary text-center installButton" id="" style="display:none;"><?php 
+
+echo $installpwa;?></button></div>
         </div>
         <!-- Portfolio Modals-->
 <script>
