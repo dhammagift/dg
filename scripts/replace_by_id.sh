784d55de39edf96ed743348925679d16c697a783
fold4 <of4@dg.co.>
2026-06-09 14:07:29 -0400

    upd replace

--- scripts/replace_by_id.sh
+++ scripts/replace_by_id.sh
@@ -8,36 +8,27 @@ rule="$2"
 # Извлекаем искомую строку из правила (текст между первым и вторым слэшем)
 search_text=$(echo "$rule" | cut -d'/' -f2)
 
-while read -r raw_id; do
-  # 1. Очищаем ID: удаляем кавычки, \r, висящие в конце двоеточия/запятые и лишние пробелы
-  # Внутреннее двоеточие в ID (например, sn45.30:1.9) при этом сохраняется
-  id=$(echo "$raw_id" | tr -d '"\r' | sed 's/[:,]*$//; s/^[[:space:]]*//; s/[[:space:]]*$//')
-  [ -z "$id" ] && continue
+awk '{print $2}' "$list" | while read -r raw_id; do
+    id=$(printf '%s\n' "$raw_id" |
+        tr -d '"\r' |
+        sed 's/[:,]*$//')
 
-  # 2. Ищем файлы, содержащие этот ID
-  while IFS= read -r file; do
-    [ -z "$file" ] && continue
-    
-    # 3. Получаем конкретную строку с ID из файла
-    target_line=$(grep "\"$id\"" "$file")
-    
-    # 4. Проверяем, содержится ли искомый текст в этой строке
-    if echo "$target_line" | grep -Fq "$search_text"; then
-        # Текст совпал, производим замену
-        sed -i "/\"$id\"/ s${rule}" "$file"
-        echo "[DONE] $id in $file"
-    else
-        # Текст не совпал (разные пробелы, тире, опечатки или текст уже заменен)
-        echo "[SKIP] not found $id in $file"
-    fi
-    
-  done < <(grep -rl "\"$id\"" assets/texts/ru/ || true)
+    [ -z "$id" ] && continue
 
-done < "$list"
-
+    grep -rl "\"$id\"" assets/texts/ru/ | while read -r file; do
+        if grep "\"$id\"" "$file" | grep -Fq "$search_text"; then
+            sed -i "/\"$id\"/ s${rule}" "$file"
+            echo "[DONE] $id in $file"
+        else
+            echo "[SKIP] $id in $file"
+        fi
+    done
+done
 
 
 exit 0
+
+cat $dwnl/id.txt | awk '{print $2}' | grep -rilf - .
 
 
 #!/usr/bin/env bash
