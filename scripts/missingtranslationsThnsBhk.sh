27e95554ae12b036f062dfc93eba1f415337a575
fold4 <of4@dg.co.>
2026-06-19 22:52:46 -0400

    upd mv and check sh

---
+++ scripts/missingtranslationsThnsBhk.sh
@@ -0,0 +1,74 @@
+
+if [[ "$@" == "" ]]  || [[ "$@" == "-o" ]] 
+then
+snrange=$(eval echo an{1..11})
+nikaya=an
+elif [[ "$@" == *"sn" ]]  
+then
+snrange=$(eval echo sn{1..56})
+nikaya=sn
+else
+snrange=$(echo $@ |  sed 's/[[:space:]]*-o[[:space:]]*//g' )
+nikaya=$(echo "$snrange" | sed 's/[0-9]//g; s/[[:space:]]*-o[[:space:]]*//g')
+
+#nikaya=$(echo $snrange | sed 's/[0-9]//g' | sed 's/\s-o\s//g' )
+fi
+
+if [[ "$@" = *"-o"* ]]; then
+    sortMe="-k1,1V -k2,2n"  # по текстовому индексу
+else
+    sortMe="-k2,2n -k1,1V"  # по строкам
+fi
+
+for sanyutta in $snrange
+do
+
+#делаем список переводов
+find ./assets/texts/en_other/sutta/$nikaya/$sanyutta/ -type f | sed 's@_.*@_@g' | awk -F'/' '{print $NF}' | uniq > trnList
+
+cat ./assets/texts/allRootTextWithLineCount | grep -E "$sanyutta\." | grep -v -f trnList  > missing
+
+#echo $sanyutta `wc -l missing | awk '{print $2, $1}'`
+cat missing | sed 's/_//g' | awk '{print $2, $1}' | sort -n | awk '{print $2, $1}' | tac | head -n1 > ./assets/texts/miss_longest
+if [[ "$@" = *"-o"* ]]; then
+	cat missing | sed 's/_//g' | tac # по текстовому индексу
+else
+	cat missing | sed 's/_//g' | awk '{print $2, $1}' | sort -n | awk '{print $2, $1}' | tac
+fi
+
+echo $sanyutta `wc -l missing | awk '{print $2, $1}'`
+
+rm missing trnList
+done 
+
+exit 0
+
+if [[ "$@" == "" ]]
+then
+snrange=$(eval echo sn{1..56})
+nikaya=sn
+else
+snrange=$@
+nikaya=$(echo $snrange | sed 's/[0-9]//g')
+fi
+
+for sanyutta in $snrange
+do
+echo $sanyutta
+for i in `find  ../suttacentral.net/sc-data/sc_bilara_data/root/pli/ms/sutta/$nikaya/$sanyutta/  -type f | awk -F'/' '{print $NF}' | sed 's/_.*//g' | sort -V | uniq ` ; do
+ls assets/texts/ru/sutta/$nikaya/$sanyutta/${i}_* 2>&1 | grep "ls:" | awk -F'/' '{print $NF}' | sed 's/_.*/ missing/g' | tee out
+done  | sort -V > total_missing
+cat total_missing
+wc -l total_missing
+done 
+
+rm total_missing
+exit 0 
+
+
+
+
+for i in `find  ../suttacentral.net/sc-data/sc_bilara_data/root/pli/ms/sutta/$nikaya/$sanyutta/  -type f | sort -V | uniq` ; do 
+echo -n "`echo $i | sed 's@_root-pli-ms.json@@g' | awk -F'/' '{print $NF}'` `cat $i | wc -l`" 
+echo  
+done | sort  -k2,2n -k1,1V
---
+++ scripts/mvNewTextsFromDownloadsThnsBhk.sh
@@ -0,0 +1,132 @@
+source ./config/script_config.sh --source-only
+
+#downloaddir=/media/c/Users/o28o/Downloads
+#trndir=/media/c/soft/dg/assets/texts/ru/sutta/
+trndir=/data/data/com.termux/files/usr/share/apache2/default-site/htdocs/assets/texts/en_other/sutta/
+thtrndir=/data/data/com.termux/files/usr/share/apache2/default-site/htdocs/assets/texts/en_other/sutta/
+
+cd $downloaddir
+for file in `find . -maxdepth 1 -type f -size +0 -name "*-en-*.json" 2>/dev/null`
+do
+newname=$(echo $file | sed 's@-ru-sv@-en-thanissaro@g')
+echo renamed $file $newname
+mv $file $newname
+done
+
+cd - 2>&1 >/dev/null
+
+#find "$downloaddir" -maxdepth 1 -type f -size 0 -name "*translation*.json" | xargs rm
+
+#en trn 
+for file in `find "$downloaddir" "$downloaddir/Telegram" -maxdepth 1 -type f -size +0 -name "*translation-en-*.json" 2>/dev/null`
+do 
+suttaname=$(echo $file | sed -E 's/_translation.*//' | awk -F'/' '{print $NF}')
+
+if [[ $suttaname =~ snp|iti|thig|thag|ud ]]; then
+nikaya=kn/$(echo "$suttaname" | sed -E 's/[0-9]+.*//')
+booknumber=vagga$(echo $suttaname | sed -E 's/\..*//' | sed 's/[a-z]*//g')
+elif [[ $suttaname =~ dhp ]]; 
+then
+nikaya=kn/$(echo "$suttaname" | sed -E 's/[0-9]+.*//')
+booknumber=''
+elif [[ $suttaname =~ mn|dn ]]; 
+then
+booknumber=
+    nikaya=$(echo "$suttaname" | sed -E 's/[0-9]+.*//')
+
+
+else
+booknumber=$(echo $suttaname | sed -E 's/\..*//')
+    nikaya=$(echo "$suttaname" | sed -E 's/[0-9]+.*//')
+fi
+
+mv $file $trndir/$nikaya/$booknumber/
+echo "moved $suttaname to ./$nikaya/$booknumber" 
+done
+
+#thai trn
+for file in `find "$downloaddir" "$downloaddir/Telegram" -maxdepth 1 -type f -size +0 -name "*translation-th-*.json" 2>/dev/null`
+do 
+suttaname=$(echo $file | sed -E 's/_translation.*//' | awk -F'/' '{print $NF}')
+
+if [[ $suttaname =~ snp|iti|thig|thag|ud ]]; then
+nikaya=kn/$(echo "$suttaname" | sed -E 's/[0-9]+.*//')
+booknumber=vagga$(echo $suttaname | sed -E 's/\..*//' | sed 's/[a-z]*//g')
+elif [[ $suttaname =~ dhp ]]; 
+then
+nikaya=kn/$(echo "$suttaname" | sed -E 's/[0-9]+.*//')
+booknumber=''
+
+elif [[ $suttaname =~ mn|dn ]]; 
+then
+booknumber=
+    nikaya=$(echo "$suttaname" | sed -E 's/[0-9]+.*//')
+
+else
+echo elsecase
+booknumber=$(echo $suttaname | sed -E 's/\..*//')
+    nikaya=$(echo "$suttaname" | sed -E 's/[0-9]+.*//')
+fi
+mkdir $thtrndir/$nikaya/$booknumber/ 2>/dev/null
+mv $file $thtrndir/$nikaya/$booknumber/
+echo "moved $suttaname to thai ./$nikaya/$booknumber" 
+done
+
+
+
+if [[ "`uname -a`" != *"Android"* ]]; then 
+exit 0
+fi
+
+audioSrcdir=/storage/emulated/0/Music/VoiceChangerWithEffects
+audioSrcdir2='/storage/emulated/0/Recordings/Voice\ Recorder'
+audioDestdir=/data/data/com.termux/files/usr/share/apache2/default-site/htdocs/assets/audio
+
+
+# Проверяем существование папок и присваиваем переменные
+[ -d "$audioSrcdir" ] || audioSrcdir=""
+[ -d "$audioSrcdir2" ] || audioSrcdir2=""
+
+# Если обе переменные пусты, выходим
+if [[ -z "$audioSrcdir" && -z "$audioSrcdir2" ]]; then
+   # echo "Обе папки отсутствуют. Выход."
+    exit 1
+fi
+
+
+dirs="$audioSrcdir $audioSrcdir2"
+find "$dirs" -maxdepth 1 -size +0 -type f -name "*_*" 2>/dev/null | while IFS= read -r file 
+do
+suttaname=$(echo "$file" | sed -E 's/_.*//' | awk -F'/' '{print $NF}')
+
+if [[ $suttaname =~ snp|iti|thig|thag|ud ]]; then
+    nikaya=kn/$(echo "$suttaname" | sed -E 's/[0-9]+.*//')
+booknumber=vagga$(echo $suttaname | sed -E 's/\..*//' | sed 's/[a-z]*//g')
+elif [[ $suttaname =~ mn|dn ]]; then
+    nikaya=$(echo "$suttaname" | sed -E 's/[0-9]+.*//')
+booknumber=
+else
+booknumber=$(echo $suttaname | sed -E 's/\..*//')
+    nikaya=$(echo "$suttaname" | sed -E 's/[0-9]+.*//')
+fi
+
+if [[ "$file" =~ "Recorder" ]]; then
+newfn=$(echo "$file" | sed 's/_.*/_pi_puriso_jiv.m4a/g')
+else
+newfn=$(echo "$file" | sed 's/_.*/_pi_puriso_mod.mp3/g')
+fi 
+mv "$file" "$newfn" >/dev/null 2>&1
+mv "$newfn" $audioDestdir/$nikaya/$booknumber/
+echo "moved $suttaname to audio/$nikaya/$booknumber" 
+done
+
+
+cd $apachesitepath/assets/texts/ru/sutta
+ for i in `find . -type f  | awk -F'_' '{print $1}' | sort -V| uniq -c | sort -V | awk '{print $1, $2}' | grep -v "^1" | awk '{print $2}'` 
+ do 
+ ls ${i}_*  
+mv ${i}_*sv.json ../svEtc/automatic/
+ done
+
+
+exit 0
