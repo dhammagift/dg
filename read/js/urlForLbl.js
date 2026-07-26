ae881db759e5779f6ca1ae8b0b5a1facd27ec850
o-tab <o-tab@dg.dg>
2026-07-26 03:23:22 -0400

    upd lbl target blank

--- read/js/urlForLbl.js
+++ read/js/urlForLbl.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 finalUrl += (baseUrl.includes("?") ? "&" : "?") + "q=" + encodeURIComponent(q);
             }
 
-            window.location.href = finalUrl;
+            window.open(finalUrl, "_blank", "noopener,noreferrer");
         });
     });
 
