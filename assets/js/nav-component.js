61b04424f988eced023bc061840c16a43517d96a
fold4 <of4@dg.co.>
2026-07-28 22:02:02 -0400

    lbl: added corpora link

--- assets/js/nav-component.js
+++ assets/js/nav-component.js
@@ -10,7 +10,7 @@ class TopNavIcons extends HTMLElement {
 
         let html = `
             <a href="/read.php" id="${firstIconId}" title="${firstIconTitle}" rel="noreferrer" class="me-1 top-nav-icon-link">
-                <svg fill="#979797" version="1.1" viewBox="0 0 547.596 547.596" height="26px" stroke="#979797"><path d="M540.76,254.788L294.506,38.216c-11.475-10.098-30.064-10.098-41.386,0L6.943,254.788 c-11.475,10.098-8.415,18.284,6.885,18.284h75.964v221.773c0,12.087,9.945,22.108,22.108,22.108h92.947V371.067 c0-12.087,9.945-22.108,22.109-22.108h93.865c12.239,0,22.108,9.792,22.108,22.108v145.886h92.947 c12.24,0,22.108-9.945,22.108-22.108v-221.85h75.965C549.021,272.995,552.081,264.886,540.76,254.788z"></path></svg>
+          <img class="common-size-icon sankha" alt="page for reading" src="/assets/img/dgsankhaonly.png">
             </a>
             <a href="/" id="nav_search_link" title="Sutta and Vinaya search" rel="noreferrer" class="me-1 top-nav-icon-link">
                 <img width="24px" alt="dhamma.gift icon" src="/assets/img/gray-white.png">
